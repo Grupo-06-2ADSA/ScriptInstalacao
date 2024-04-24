09520b779ccbdaba85a6cf13d1639486e73f6c15
KevinSampaio <kevin.sampaio@sptech.school>
2024-04-24 17:15:15 -0300

    V2 SH

--- script.sh
+++ script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo "$(tput setaf 5)[Instalador MindCore]: $(tput sgr0)$(tput setaf 10)Olá, te ajudarei a instalar nossa aplicação!"
+sleep 2
+
+echo "$(tput setaf 5)[Instalador MindCore]: $(tput sgr0)$(tput setaf 10)Primeiro irei atualizar os pacotes do seu sistema."
+sleep 2
+
 sudo apt update && sudo apt upgrade -y
+
+echo "$(tput setaf 5)[Instalador MindCore]: $(tput sgr0) $(tput setaf 10)Irei verificar se você já tem o Java."
+sleep 2
 
 java -version #verifica versao atual do java
 
