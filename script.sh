2306d3a8ed26adda743b1ee6fe4926443076d707
KevinSampaio <kevin.sampaio@sptech.school>
2024-04-24 17:25:22 -0300

    Testes

--- script.sh
+++ script.sh
@@ -11,18 +11,18 @@ sudo apt update && sudo apt upgrade -y
 echo "$(tput setaf 5)[Instalador MindCore]: $(tput sgr0) $(tput setaf 10)Irei verificar se você já tem o Java."
 sleep 2
 
-java -version #verifica versao atual do java
+java --version
 
-if [ $? = 0 ]; #se retorno for igual a 0
-	then #entao,
-	echo “java instalado” #print no terminal
+if [ $? = 0 ]; 
+	then 
+	echo “java instalado” 
 else #se nao,
-	echo “java não instalado” #print no terminal
-	echo “gostaria de instalar o java? [s/n]” #print no terminal
+	echo “java não instalado” 
+	echo “gostaria de instalar o java? [s/n]” 
 
-	read get #variável que guarda resposta do usuário
-	if [ \“$get\” == \“s\” ]; #se retorno for igual a s
-		then #entao
-		sudo apt install openjdk-17-jre -y #executa instalacao do java
-	fi #fecha o 2º if
-fi #fecha o 1º if
+	read get 
+	if [ \“$get\” == \“s\” ]; 
+		then 
+		sudo apt install openjdk-17-jre -y 
+	fi 
+fi 
